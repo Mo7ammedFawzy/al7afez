b4a771d3107ca476e0ff63c22e7b52b7cb82ce2e
lith <abdelrahmanlith8923@gmail.com>
2026-05-10 00:31:58 +0300

    -Using al7fez theme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,29 @@ import "./style.css";
 
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+import { definePreset } from "@primeuix/themes";
 import ToastService from "primevue/toastservice";
 import ConfirmationService from "primevue/confirmationservice";
 import "primeicons/primeicons.css";
+
+const Al7afezTheme = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50:  "#eef4ff",
+      100: "#d8e4ff",
+      200: "#b5cbff",
+      300: "#83a8ff",
+      400: "#6791ff",
+      500: "#2f65d5",
+      600: "#2554b8",
+      700: "#1d4296",
+      800: "#163275",
+      900: "#102358",
+      950: "#0a1637",
+    },
+    fontFamily: "'Cairo', 'Segoe UI', sans-serif",
+  },
+});
 
 import StudentsView from "./views/StudentsView.vue";
 import SheikhsView from "./views/SheikhsView.vue";
@@ -49,7 +69,7 @@ app.use(router);
 app.use(i18n);
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: Al7afezTheme,
     options: {
       darkModeSelector: ".dark",
     },
